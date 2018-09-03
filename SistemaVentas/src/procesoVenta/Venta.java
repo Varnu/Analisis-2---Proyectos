608169a376af87db5bcbf50f57265f8d3dd23080
Unknown <jufegare@gmail.com>
2018-09-03 10:47:29 -0500

    [Sistema de ventas] - Setter y getter de Venta

--- SistemaVentas/src/procesoVenta/Venta.java
+++ SistemaVentas/src/procesoVenta/Venta.java
@@ -13,4 +13,40 @@ public class Venta {
     private int id;
     private String fechaVenta;
     private double total;
+
+    public Venta(int id, String fechaVenta, double total) {
+        this.id = id;
+        this.fechaVenta = fechaVenta;
+        this.total = total;
+    }
+    
+    public Venta(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
 }
