e58ce8191bc4cd3f0d274b3aa678e3e9dc901056
Unknown <alejandrom.montoya@udea.edu.co>
2018-09-03 11:11:40 -0500

    impuestos

--- SistemaVentas/src/procesoVenta/Producto.java
+++ SistemaVentas/src/procesoVenta/Producto.java
@@ -12,4 +12,21 @@ package procesoVenta;
 public class Producto {
     private String id;
     private String nombre;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 }
--- SistemaVentas/src/procesoVenta/linea.java
+++ SistemaVentas/src/procesoVenta/linea.java
@@ -13,5 +13,21 @@ public class linea {
     
     private int cantidad;
     private Producto producto;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
     
 }
