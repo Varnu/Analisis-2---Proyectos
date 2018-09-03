3695805bb8132087fbde55c1e257d8e9a693937f
Unknown <jairocampana10001@gmail.com>
2018-09-03 10:49:19 -0500

    [Sistema de Ventas] Cliente y persona class

---
+++ SistemaVentas/src/procesoVenta/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procesoVenta;
+
+/**
+ *
+ * @author Study-Development
+ */
+public abstract class Persona {
+    String nombre;
+    String apellido;
+    short edad;
+    String id;
+    
+    
+}
