4814813acb473c5bb3e8651f67b300de5b37bd93
Unknown <jufegare@gmail.com>
2018-09-03 10:41:08 -0500

    Nuevo pakete sistema de ventas
    Acá se agrega el paquete para comenzar implemetación de código

---
+++ SistemaVentas/src/procesoVenta/Venta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package procesoVenta;
+
+/**
+ *
+ * @author juanf.gallo
+ */
+public class Venta {
+    private int id;
+    private String fechaVenta;
+    private double total;
+}
